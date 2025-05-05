1fd904d34dca2543b7feb6893c5cc9651aa8534f
wladyslaw ziemkiewicz <wladyslaw.ziemkiewicz@wartsila.com>
2025-05-05 13:11:52 +0200

    update

--- Insert.sql
+++ Insert.sql
@@ -1,40 +1,35 @@
 -- Insert data for KUNDE table
-INSERT INTO KUNDE (Vorname, Nachnamen, Geburtsdatum, Telefon, Email) VALUES 
-('Kira', 'Wellensteyn', '1990-05-05', '040/443322', 'k.wellensteyn@yahoo.de'),
-('Dorothea', 'Foede', '2000-03-24', '040/543822', 'd.foede@web.de'),
-('Sigrid', 'Leberer', '1989-09-21', '0175/1234588', 'sigrid@leberer.de'),
-('Hanna', 'Soerensen', '1974-04-03', '040/634578', 'h.soerensen@yahoo.de'),
-('Marten', 'Schnitter', '1964-04-17', '0176/447587', 'schni_mart@gmail.com'),
-('Belinda', 'Maurer', '1978-09-09', '040/332189', 'belinda1978@yahoo.de'),
-('Armin', 'Gessert', '1978-01-29', '040/67890', 'armin@gessert.de'),
-('Jean-Marc', 'Haessig', '1982-08-30', '0178-67013390', 'jm@haessig.de'),
-('Eric', 'Urocki', '1999-12-04', '0152-96701390', 'urocki@outlook.de');
+INSERT INTO KUNDE (KdID, Vorname, Nachnamen, Geburtsdatum, Telefon, Email) VALUES 
+(2001, 'Kira', 'Wellensteyn', '1990-05-05', '040/443322', 'k.wellensteyn@yahoo.de'),
+(2002, 'Dorothea', 'Foede', '2000-03-24', '040/543822', 'd.foede@web.de'),
+(2003, 'Sigrid', 'Leberer', '1989-09-21', '0175/1234588', 'sigrid@leberer.de'),
+(2004, 'Hanna', 'Soerensen', '1974-04-03', '040/634578', 'h.soerensen@yahoo.de'),
+(2005, 'Marten', 'Schnitter', '1964-04-17', '0176/447587', 'schni_mart@gmail.com'),
+(2006, 'Belinda', 'Maurer', '1978-09-09', '040/332189', 'belinda1978@yahoo.de'),
+(2007, 'Armin', 'Gessert', '1978-01-29', '040/67890', 'armin@gessert.de'),
+(2008, 'Jean-Marc', 'Haessig', '1982-08-30', '0178-67013390', 'jm@haessig.de'),
+(2009, 'Eric', 'Urocki', '1999-12-04', '0152-96701390', 'urocki@outlook.de');
 
 -- Insert data for LIEFERANT table
-INSERT INTO LIEFERANT (Name) VALUES 
-('Gemüsegroßhandel Frisch & Co'),
-('Fleischerei Müller'),
-('Bio-Hof Schmidt'),
-('Molkerei Alpenglück'),
-('Fischgroßhandel Meeresbrise'),
-('Bio-Hof Müller'),
-('Obst-Hof Altes Land'),
-('Molkerei Henning');
+INSERT INTO LIEFERANT (LfID, Name) VALUES 
+(101, 'Bio-Hof Müller'),
+(102, 'Obst-Hof Altes Land'),
+(103, 'Molkerei Henning');
 
 -- Insert data for ADRESSE table
 INSERT INTO ADRESSE (PLZ, Stadt, Strasse, HausNr, KdID, LfID) VALUES
-(20249, 'Hamburg', 'Eppendorfer Landstrasse', 104, 1, NULL),
-(22765, 'Hamburg', 'Ohmstraße', 23, 2, NULL),
-(20459, 'Hamburg', 'Bilser Berg', 6, 3, NULL),
-(22049, 'Hamburg', 'Alter Teichweg', 95, 4, NULL),
-(22835, 'Barsbüttel', 'Stübels', 10, 5, NULL),
-(21075, 'Hamburg', 'Grotelertwiete', 4, 6, NULL),
-(21079, 'Hamburg', 'Küstersweg', 3, 7, NULL),
-(21149, 'Hamburg', 'Neugrabener Bahnhofstraße', 30, 8, NULL),
-(22605, 'Hamburg', 'Elbchaussee', 228, 9, NULL),
-(24354, 'Weseby', 'Dorfstraße', 74, NULL, 6),
-(21635, 'Jork', 'Westerjork 74', 76, NULL, 7),
-(19217, 'Dechow', 'Molkereiwegkundekunde', 13, NULL, 8);
+(20249, 'Hamburg', 'Eppendorfer Landstrasse', 104, 2001, NULL),
+(22765, 'Hamburg', 'Ohmstraße', 23, 2002, NULL),
+(20459, 'Hamburg', 'Bilser Berg', 6, 2003, NULL),
+(22049, 'Hamburg', 'Alter Teichweg', 95, 2004, NULL),
+(22835, 'Barsbüttel', 'Stübels', 10, 2005, NULL),
+(21075, 'Hamburg', 'Grotelertwiete', 4, 2006, NULL),
+(21079, 'Hamburg', 'Küstersweg', 3, 2007, NULL),
+(21149, 'Hamburg', 'Neugrabener Bahnhofstraße', 30, 2008, NULL),
+(22605, 'Hamburg', 'Elbchaussee', 228, 2009, NULL),
+(24354, 'Weseby', 'Dorfstraße', 74, NULL, 101),
+(21635, 'Jork', 'Westerjork 74', 76, NULL, 102),
+(19217, 'Dechow', 'Molkereiwegkundekunde', 13, NULL, 103);
 
 -- Insert data for ALLERGEN table
 INSERT INTO ALLERGEN (Bezeichnung) VALUES 
@@ -56,66 +51,66 @@ INSERT INTO ERNÄHRUNGSKATEGORIE (Bezeichnung) VALUES
 -- Insert data for ZUTAT table
 INSERT INTO ZUTAT (Bezeichnung, Bestand, FettProHundertGramm, BallaststoffeProHundertGramm, NatriumProHundertGramm, Co2Bilanz, KalorienProHundertGramm, Nettopreis, KohlenhydrateProHundertGramm, Einheit, LfID) VALUES 
 -- Milchreis-Zutaten
-('Vollmilch', 500, 3.50, 0.00, 0.05, 1.30, 65, 1.25, 4.80, 'Liter', 4),
-('Milchreis', 300, 0.50, 1.30, 0.01, 0.80, 350, 1.99, 78.00, 'kg', 3),
-('Zucker', 800, 0.00, 0.00, 0.00, 0.60, 400, 0.89, 100.00, 'kg', 3),
-('Butter', 400, 82.00, 0.00, 0.10, 7.30, 741, 2.49, 0.60, 'kg', 4),
-('Vanillezucker', 150, 0.00, 0.00, 0.00, 0.60, 390, 0.39, 95.00, 'Päckchen', 3),
-('Apfelmus', 200, 0.20, 2.00, 0.01, 0.90, 83, 1.29, 20.00, 'Glas', 3),
+('Vollmilch', 500, 3.50, 0.00, 0.05, 1.30, 65, 1.25, 4.80, 'Liter', 103),
+('Milchreis', 300, 0.50, 1.30, 0.01, 0.80, 350, 1.99, 78.00, 'kg', 101),
+('Zucker', 800, 0.00, 0.00, 0.00, 0.60, 400, 0.89, 100.00, 'kg', 101),
+('Butter', 400, 82.00, 0.00, 0.10, 7.30, 741, 2.49, 0.60, 'kg', 103),
+('Vanillezucker', 150, 0.00, 0.00, 0.00, 0.60, 390, 0.39, 95.00, 'Päckchen', 101),
+('Apfelmus', 200, 0.20, 2.00, 0.01, 0.90, 83, 1.29, 20.00, 'Glas', 102),
 
 -- Couscous-Salat-Zutaten
-('Couscous', 250, 1.40, 5.00, 0.01, 0.70, 365, 1.79, 75.00, 'kg', 3),
-('Gemüsebrühe', 180, 0.50, 0.00, 0.70, 0.30, 15, 2.49, 3.00, 'Liter', 3),
-('Tomaten', 350, 0.20, 1.50, 0.01, 0.40, 18, 2.99, 3.90, 'kg', 1),
-('Zucchini', 280, 0.30, 1.00, 0.00, 0.30, 17, 2.29, 3.10, 'kg', 1),
-('Paprika gelb', 150, 0.20, 2.00, 0.00, 0.40, 27, 3.99, 6.40, 'kg', 1),
-('Kräuter gemischt', 100, 0.50, 3.00, 0.02, 0.20, 35, 1.99, 5.00, 'Bund', 3),
-('Pflanzenöl', 400, 100.00, 0.00, 0.00, 3.10, 900, 2.49, 0.00, 'Liter', 3),
-('Salz', 1000, 0.00, 0.00, 39.00, 0.10, 0, 0.49, 0.00, 'kg', 3),
-('Pfeffer', 500, 3.30, 25.00, 0.04, 0.20, 255, 1.99, 64.00, 'kg', 3),
+('Couscous', 250, 1.40, 5.00, 0.01, 0.70, 365, 1.79, 75.00, 'kg', 101),
+('Gemüsebrühe', 180, 0.50, 0.00, 0.70, 0.30, 15, 2.49, 3.00, 'Liter', 101),
+('Tomaten', 350, 0.20, 1.50, 0.01, 0.40, 18, 2.99, 3.90, 'kg', 101),
+('Zucchini', 280, 0.30, 1.00, 0.00, 0.30, 17, 2.29, 3.10, 'kg', 101),
+('Paprika gelb', 150, 0.20, 2.00, 0.00, 0.40, 27, 3.99, 6.40, 'kg', 101),
+('Kräuter gemischt', 100, 0.50, 3.00, 0.02, 0.20, 35, 1.99, 5.00, 'Bund', 101),
+('Pflanzenöl', 400, 100.00, 0.00, 0.00, 3.10, 900, 2.49, 0.00, 'Liter', 101),
+('Salz', 1000, 0.00, 0.00, 39.00, 0.10, 0, 0.49, 0.00, 'kg', 101),
+('Pfeffer', 500, 3.30, 25.00, 0.04, 0.20, 255, 1.99, 64.00, 'kg', 101),
 
 -- Lachslasagne-Zutaten
-('Lasagneplatten', 180, 1.50, 3.00, 0.02, 0.90, 350, 1.29, 71.00, 'kg', 3),
-('Spinat', 200, 0.40, 2.20, 0.07, 0.30, 23, 1.89, 3.60, 'kg', 1),
-('Lachs', 150, 13.00, 0.00, 0.05, 5.20, 208, 19.99, 0.00, 'kg', 5),
-('Käse gerieben', 200, 25.00, 0.00, 0.70, 8.40, 350, 3.99, 1.00, 'kg', 4),
-('Mehl', 500, 1.00, 3.50, 0.00, 0.70, 348, 0.79, 72.00, 'kg', 3),
-('Sahne', 300, 30.00, 0.00, 0.04, 2.70, 204, 1.49, 3.00, 'Liter', 4),
-('Parmesan', 120, 29.00, 0.00, 1.60, 8.50, 431, 4.99, 0.00, 'kg', 4),
-('Zitrone', 180, 0.20, 2.80, 0.00, 0.30, 29, 0.49, 9.00, 'Stück', 1),
-('Muskat', 100, 36.00, 21.00, 0.03, 0.70, 525, 1.29, 49.00, 'Packung', 3),
+('Lasagneplatten', 180, 1.50, 3.00, 0.02, 0.90, 350, 1.29, 71.00, 'kg', 101),
+('Spinat', 200, 0.40, 2.20, 0.07, 0.30, 23, 1.89, 3.60, 'kg', 101),
+('Lachs', 150, 13.00, 0.00, 0.05, 5.20, 208, 19.99, 0.00, 'kg', 101),
+('Käse gerieben', 200, 25.00, 0.00, 0.70, 8.40, 350, 3.99, 1.00, 'kg', 103),
+('Mehl', 500, 1.00, 3.50, 0.00, 0.70, 348, 0.79, 72.00, 'kg', 101),
+('Sahne', 300, 30.00, 0.00, 0.04, 2.70, 204, 1.49, 3.00, 'Liter', 103),
+('Parmesan', 120, 29.00, 0.00, 1.60, 8.50, 431, 4.99, 0.00, 'kg', 103),
+('Zitrone', 180, 0.20, 2.80, 0.00, 0.30, 29, 0.49, 9.00, 'Stück', 102),
+('Muskat', 100, 36.00, 21.00, 0.03, 0.70, 525, 1.29, 49.00, 'Packung', 101),
 
 -- Thaicurry-Zutaten
-('Hähnchenbrustfilet', 200, 3.60, 0.00, 0.09, 4.20, 165, 7.99, 0.00, 'kg', 2),
-('Reis', 300, 0.70, 1.30, 0.00, 2.70, 350, 1.99, 78.00, 'kg', 3),
-('Sojasauce', 250, 0.10, 0.00, 5.70, 0.80, 53, 2.49, 8.00, 'Flasche', 3),
-('Knoblauchzehe', 150, 0.50, 1.70, 0.02, 0.20, 149, 0.99, 33.00, 'kg', 1),
-('Gemischtes Gemüse', 200, 0.30, 3.00, 0.02, 0.40, 25, 3.99, 5.00, 'kg', 1),
-('Bambussprossen', 100, 0.30, 2.20, 0.01, 0.60, 19, 1.79, 3.00, 'Glas', 3),
-('Kokosmilch', 180, 17.00, 0.00, 0.03, 0.90, 230, 1.99, 5.50, 'Dose', 3),
-('Currypaste grün', 90, 4.00, 1.20, 1.80, 0.50, 115, 2.49, 15.00, 'Glas', 3),
+('Hähnchenbrustfilet', 200, 3.60, 0.00, 0.09, 4.20, 165, 7.99, 0.00, 'kg', 101),
+('Reis', 300, 0.70, 1.30, 0.00, 2.70, 350, 1.99, 78.00, 'kg', 101),
+('Sojasauce', 250, 0.10, 0.00, 5.70, 0.80, 53, 2.49, 8.00, 'Flasche', 101),
+('Knoblauchzehe', 150, 0.50, 1.70, 0.02, 0.20, 149, 0.99, 33.00, 'kg', 101),
+('Gemischtes Gemüse', 200, 0.30, 3.00, 0.02, 0.40, 25, 3.99, 5.00, 'kg', 101),
+('Bambussprossen', 100, 0.30, 2.20, 0.01, 0.60, 19, 1.79, 3.00, 'Glas', 101),
+('Kokosmilch', 180, 17.00, 0.00, 0.03, 0.90, 230, 1.99, 5.50, 'Dose', 102),
+('Currypaste grün', 90, 4.00, 1.20, 1.80, 0.50, 115, 2.49, 15.00, 'Glas', 101),
 
 -- Kartoffelsuppe-Zutaten
-('Kartoffeln', 500, 0.10, 2.00, 0.01, 0.20, 77, 1.99, 17.00, 'kg', 1),
-('Karotte', 300, 0.20, 3.00, 0.06, 0.10, 41, 1.49, 10.00, 'kg', 1),
-('Lauch', 150, 0.30, 2.10, 0.05, 0.20, 29, 2.29, 6.00, 'kg', 1),
-('Zwiebel', 200, 0.10, 1.70, 0.01, 0.10, 40, 0.99, 9.00, 'kg', 1),
-('Wiener Würstchen', 100, 25.00, 0.00, 0.80, 7.20, 280, 3.99, 2.00, 'Paar', 2),
-('Tofuwürstchen', 80, 8.00, 4.00, 0.40, 1.20, 160, 2.99, 10.00, 'Paar', 3),
-('Petersilie', 120, 0.40, 3.30, 0.05, 0.10, 36, 0.99, 6.30, 'Bund', 1),
-('Schnittlauch', 100, 0.70, 2.50, 0.03, 0.10, 30, 0.99, 4.50, 'Bund', 1),
-('Majoran', 90, 7.00, 40.00, 0.05, 0.20, 271, 1.29, 61.00, 'Packung', 3);
+('Kartoffeln', 500, 0.10, 2.00, 0.01, 0.20, 77, 1.99, 17.00, 'kg', 101),
+('Karotte', 300, 0.20, 3.00, 0.06, 0.10, 41, 1.49, 10.00, 'kg', 101),
+('Lauch', 150, 0.30, 2.10, 0.05, 0.20, 29, 2.29, 6.00, 'kg', 101),
+('Zwiebel', 200, 0.10, 1.70, 0.01, 0.10, 40, 0.99, 9.00, 'kg', 101),
+('Wiener Würstchen', 100, 25.00, 0.00, 0.80, 7.20, 280, 3.99, 2.00, 'Paar', 101),
+('Tofuwürstchen', 80, 8.00, 4.00, 0.40, 1.20, 160, 2.99, 10.00, 'Paar', 101),
+('Petersilie', 120, 0.40, 3.30, 0.05, 0.10, 36, 0.99, 6.30, 'Bund', 101),
+('Schnittlauch', 100, 0.70, 2.50, 0.03, 0.10, 30, 0.99, 4.50, 'Bund', 101),
+('Majoran', 90, 7.00, 40.00, 0.05, 0.20, 271, 1.29, 61.00, 'Packung', 101);
 
 -- Zusätzliche Zutaten aus paste-2.txt
 INSERT INTO ZUTAT (Bezeichnung, Bestand, FettProHundertGramm, BallaststoffeProHundertGramm, NatriumProHundertGramm, Co2Bilanz, KalorienProHundertGramm, Nettopreis, KohlenhydrateProHundertGramm, Einheit, LfID) VALUES 
-('Rucola', 10, 0.40, 2.60, 0.05, 0.30, 27, 0.90, 2.10, 'Bund', 1),
-('Knoblauch', 250, 0.50, 6.10, 0.02, 0.20, 141, 0.25, 28.40, 'Stück', 1),
-('Basilikum', 10, 0.50, 3.10, 0.02, 0.20, 41, 1.30, 5.10, 'Bund', 1),
-('Süßkartoffel', 200, 0.30, 1.60, 0.01, 0.40, 86, 2.00, 20.00, 'Stück', 1),
-('Apfel', 750, 0.20, 0.30, 0.00, 0.50, 54, 1.20, 14.40, 'Stück', 7),
-('Mozzarella', 20, 18.10, 1.00, 0.00, 5.00, 241, 3.50, 1.00, 'Packung', 8),
-('Ei', 300, 11.90, 1.50, 0.00, 3.00, 137, 0.40, 1.50, 'Stück', 7),
-('Kichererbsen', 400, 9.00, 21.20, 0.00, 0.80, 150, 1.00, 21.20, 'Dose', 8);
+('Rucola', 10, 0.40, 2.60, 0.05, 0.30, 27, 0.90, 2.10, 'Bund', 101),
+('Knoblauch', 250, 0.50, 6.10, 0.02, 0.20, 141, 0.25, 28.40, 'Stück', 101),
+('Basilikum', 10, 0.50, 3.10, 0.02, 0.20, 41, 1.30, 5.10, 'Bund', 101),
+('Süßkartoffel', 200, 0.30, 1.60, 0.01, 0.40, 86, 2.00, 20.00, 'Stück', 101),
+('Apfel', 750, 0.20, 0.30, 0.00, 0.50, 54, 1.20, 14.40, 'Stück', 102),
+('Mozzarella', 20, 18.10, 1.00, 0.00, 5.00, 241, 3.50, 1.00, 'Packung', 103),
+('Ei', 300, 11.90, 1.50, 0.00, 3.00, 137, 0.40, 1.50, 'Stück', 101),
+('Kichererbsen', 400, 9.00, 21.20, 0.00, 0.80, 150, 1.00, 21.20, 'Dose', 101);
 
 -- Insert data for ZUTAT_ALLERGEN table
 INSERT INTO ZUTAT_ALLERGEN (ZtID, AlgID) VALUES 
